e0b9546a394719015da7136845076bac617900cb
David Codina <davidmcodina@icloud.com>
2024-05-27 19:09:07 -0600

    Updated Ribbon.

--- src/pages/PageHome/index.tsx
+++ src/pages/PageHome/index.tsx
@@ -1,5 +1,5 @@
 import { useTitle } from 'hooks'
-import { HR, Waves } from 'components'
+import { HR, Waves, Ribbon } from 'components'
 import { useThemeContext } from 'contexts'
 
 const backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23ddd6fe'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`
@@ -56,6 +56,20 @@ const PageHome = () => {
         </h1>
 
         <HR style={{ marginBottom: 50 }} />
+
+        <div
+          style={{
+            margin: '0 auto',
+            height: 200,
+            position: 'relative',
+            width: 300,
+            backgroundColor: '#fff',
+            border: '1px solid #333',
+            borderRadius: 5
+          }}
+        >
+          <Ribbon>Demo</Ribbon>
+        </div>
 
         {/* <Alert className='alert-blue mx-auto mb-12 max-w-2xl flex-col'>
           <div className='text-center'>
